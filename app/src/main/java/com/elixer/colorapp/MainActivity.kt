332d72713cd5046e3386f9e60f3ef18cee429939
Shivam Dhuria <abc@Shivams-MacBook-Pro.local>
2021-12-24 01:04:00 +0530

    added bounds of the circle

--- app/src/main/java/com/elixer/colorapp/MainActivity.kt
+++ app/src/main/java/com/elixer/colorapp/MainActivity.kt
@@ -3,14 +3,18 @@ package com.elixer.colorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.elixer.colorapp.ui.theme.ColorAppTheme
+import com.elixer.palette.Palette
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +23,9 @@ class MainActivity : ComponentActivity() {
             ColorAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Palette(Color.Green, buttonSize = 80.dp, Modifier.fillMaxWidth().height(600.dp))
+                    }
                 }
             }
         }
--- palette/src/main/java/com/elixer/palette/Palette.kt
+++ palette/src/main/java/com/elixer/palette/Palette.kt
@@ -1,4 +1,55 @@
 package com.elixer.palette
 
-class Palette {
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Palette(
+    defaultColor: Color,
+    buttonSize: Dp = 80.dp,
+    modifier: Modifier
+) {
+
+    val boxSizeWidth = remember { mutableStateOf(0f) }
+    val boxSizeHeight = remember { mutableStateOf(0f) }
+
+    Canvas(modifier = modifier.fillMaxSize()) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+
+        drawRect(
+            color = Color.Green,
+            style = Stroke(50f)
+        )
+
+        drawCircle(
+            color = Color.Black,
+            radius = canvasWidth/2,
+            style = Stroke(width = 15f, cap = StrokeCap.Round)
+        )
+
+
+
+        drawCircle(
+            color = defaultColor,
+            radius = 45f,
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 500, heightDp = 500)
+@Composable
+fun PreviewPalette() {
+    Palette(defaultColor = Color.Blue, modifier = Modifier.fillMaxSize())
 }
